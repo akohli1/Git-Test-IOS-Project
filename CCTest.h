922c3254e578758e290f7ac803155e8dd3752dc9
Ayush <ayushkohli@live.com>
2014-12-21 14:55:27 -0800

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Ayush Kohli on 12/21/14.
+//  Copyright (c) 2014 Ayush Kohli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Ayush Kohli on 12/21/14.
+//  Copyright (c) 2014 Ayush Kohli. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
